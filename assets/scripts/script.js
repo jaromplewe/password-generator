799b63890579513490f860189019820d28a0ca30
Jarom Plewe <jplewe23@gmail.com>
2020-08-01 14:38:38 -0600

    fixed paths, added pseudocode

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword() {
+  console.log('generatePassword is running')
+}
+
 // Write password to the #password input
 function writePassword() {
+  console.log('writePassword is running')
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
