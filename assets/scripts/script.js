73efeadd2b3cac57faa27e9d998a053941868804
Jarom Plewe <jplewe23@gmail.com>
2020-08-02 17:12:37 -0600

    added prompts and confirms for password criteria

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,12 +3,39 @@ var generateBtn = document.querySelector("#generate");
 
 
 function generatePassword() {
-  console.log('generatePassword is running')
+  // letters array
+  var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+  // character length
+  var characterLength = prompt('How many characters? (8-128)');
+  console.log(characterLength);
+
+  // upper and lower case letters?
+  var caseYN = confirm('Would you like to include upper AND lower case letters?');
+  if (caseYN) {
+    var lettersUC = letters;
+    console.log(letters);
+  } else {
+    for (var i = 0; i < letters.length; i++) {
+      letters = letters.map(letters[i]).toLowerCase();
+      console.log(letters);
+    }
+
+  }
+
+  // symbols?
+  var symbolYN = confirm("Would you like to include symbols? (@%+\/'!#$^?:,-_.)");
+  console.log(symbolYN);
+
+  // numbers?
+  var numbersYN = confirm('Would you like to include numbers?');
+  console.log(numbersYN);
 }
+
 
 // Write password to the #password input
 function writePassword() {
-  console.log('writePassword is running')
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
---
+++ pseudo.md
@@ -0,0 +1,19 @@
+Start: user clicks on a button, "generate password"
+
+Step 2: collect user input
+* length
+    * minimum 8 characters
+    * maximum 128 characters
+* symbols
+    * what types of symbols should I use?
+* numbers
+* uppercase letters
+* lowercase letters
+
+step 3: Randomize the input and put it in an order
+* random numbers
+* select random characters
+* random letters
+    * define alphabet array?
+
+step 4: display in html
