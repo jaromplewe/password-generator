4f10d069ad82c28b0deabd3c0875f66b224be4c8
jaromplewe <66282922+jaromplewe@users.noreply.github.com>
2020-08-03 15:46:32 -0600

    cleaned symbols array

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,7 +7,7 @@ function generatePassword() {
   let lettersUC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   let lettersLC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   // symbols array
-  let symbols = ["@", "%", "+", "/", "", "!", "#", "$", "^", ":","?", ":", "-", "_", "."];
+  let symbols = ["@", "%", "+", "/", "!", "#", "$", "^", ":", "?", ":", "-", "_", "."];
   // numbers array
   let numbers = [0,1,2,3,4,5,6,7,8,9];
 
