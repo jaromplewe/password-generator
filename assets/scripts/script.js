aef9b0d38f4b225095c5ff0406a9ef64b22bea66
Jarom Plewe <jplewe23@gmail.com>
2020-08-03 01:10:10 -0600

    trying to combine all appropriate arrays with .concat

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,48 +1,59 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 
 function generatePassword() {
   // letters array
-  var lettersUC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
-  var lettersLC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+  let lettersUC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let lettersLC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   // symbols array
-  var symbols = ["@", "%", "+", "\", "/", "", "!", "#", "$", "^", :?", ":", "-", "_", "."];
+  let symbols = ["@", "%", "+", "/", "", "!", "#", "$", "^", ":","?", ":", "-", "_", "."];
+  // numbers array
+  let numbers = [0,1,2,3,4,5,6,7,8,9];
+
 
   // character length
-  var characterLength = prompt('How many characters? (8-128)');
+  let characterLength = prompt('How many characters? (8-128)');
   console.log(characterLength);
 
   // upper and lower case letters?
-  var caseYN = confirm('Would you like to include upper AND lower case letters?');
-  // return 'letters' according to answer
+  let caseYN = confirm('Would you like to include upper AND lower case letters?');
+  // return 'letters' according to answer=
   if (caseYN) {
-    var letters = [lettersUC + lettersLC];
-    console.log(letters);
+    var passwordContents = lettersUC.concat(lettersLC);
+    // console.log(letters);
   } else {
-    var letters = lettersLC;
-    console.log(letters);
+    var passwordContents = lettersLC;
+    // console.log(letters);
   }
-
+  
   // symbols?
-  var symbolYN = confirm("Would you like to include symbols? (@%+\/'!#$^?:,-_.)");
-  console.log(symbolYN);
+  let symbolYN = confirm("Would you like to include symbols? (@%+/'!#$^:?,-_.)");
   // return symbols if Y
   if (symbolYN) {
-    
+    var passwordContents = passwordContents.concat(symbols);
+    // console.log(symbolY);
   }
-
+  
+  
   // numbers?
-  var numbersYN = confirm('Would you like to include numbers?');
-  console.log(numbersYN);
+  let numbersYN = confirm('Would you like to include numbers?');
+  if (numbersYN) {
+    var passwordContents = passwordContents.concat(numbers);
+    // console.log(symbolY);
+  }
+  console.log(passwordContents);
+  
+  // CREATE THE PASSWORD
+  for (n = 0; n > characterLength.length; n++);
+  return characterLength[n];
 }
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
