e45245fe27dd18feadd2c8126a463c8f72a7e1bb
Jarom Plewe <jplewe23@gmail.com>
2020-08-06 12:14:12 -0600

    trying to ensure all criteria are met in final password with passIncludes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,7 +8,7 @@ function generatePassword() {
   var lettersLC = ['abcdefghijklmnopqrstuvwxyz'];
   var symbols = ["@%+/'!#$^:?,-_."];
   var numbers = ['0123456789'];
-  
+
   // set variables for function
   var characterLength = prompt('How many characters? (8-128)');
   var passwordContents = "";
@@ -18,26 +18,47 @@ function generatePassword() {
 
   if (confirm('Would you like to include upper case letters?')) {
     passwordContents = passwordContents.concat(lettersUC);
-  } 
-  
-  if (confirm('Would you like to include lower case letters?')){
+  }
+
+  if (confirm('Would you like to include lower case letters?')) {
     passwordContents = passwordContents.concat(lettersLC);
   }
-  
+
   if (confirm("Would you like to include symbols? (@%+/'!#$^:?,-_.)")) {
     passwordContents = passwordContents.concat(symbols);
   }
-  
+
   if (confirm('Would you like to include numbers?')) {
     passwordContents = passwordContents.concat(numbers);
   }
-  
+
   // CREATE THE PASSWORD
-  for (var n=0; n<characterLength; n++) {
-      password += passwordContents[Math.floor(Math.random() * passwordContents.length)];
+  for (var n = 0; n < characterLength; n++) {
+    password += passwordContents[Math.floor(Math.random() * passwordContents.length)];
+
+  }
+  // return (password);
+  console.log(password);
+  if(passIncludes(password, lettersUC)) {
+    return password
+  } else {
+    generatePassword();
+  }
+
+  function passIncludes(arr1, arr2) {
+
+    for(var i = 0; i < arr1.length; i++) {
+
+      for(var j = 0; j < arr2.length; j++) {
+
+        if(arr1[i] == arr2[j]) {
+          return true;
+        }
+      }
     }
-  
-    return (password); 
+    // console.log('False');
+    return false;
+  }
 
 }
 
