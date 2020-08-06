133f66d3867b9fa7a88fdcced13e4fffe135dac0
Jarom Plewe <jplewe23@gmail.com>
2020-08-06 16:31:47 -0600

    continuing to attempt guarantee of criteria in final password

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,60 +5,67 @@ let generateBtn = document.querySelector("#generate");
 function generatePassword() {
   // set arrays
   var lettersUC = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+  // var UCSplit = lettersUC.split("");
+  // console.log(UCSplit);
   var lettersLC = ['abcdefghijklmnopqrstuvwxyz'];
   var symbols = ["@%+/'!#$^:?,-_."];
-  var numbers = ['0123456789'];
+  
+  let numbers = ['0123456789'];
 
   // set variables for function
   var characterLength = prompt('How many characters? (8-128)');
   var passwordContents = "";
   var password = "";
-
+  
   // collect criteria for password
-
+  
   if (confirm('Would you like to include upper case letters?')) {
     passwordContents = passwordContents.concat(lettersUC);
   }
-
+  
   if (confirm('Would you like to include lower case letters?')) {
     passwordContents = passwordContents.concat(lettersLC);
   }
-
+  
   if (confirm("Would you like to include symbols? (@%+/'!#$^:?,-_.)")) {
     passwordContents = passwordContents.concat(symbols);
   }
-
+  
   if (confirm('Would you like to include numbers?')) {
     passwordContents = passwordContents.concat(numbers);
   }
-
-  // CREATE THE PASSWORD
+  
+  // create the password
   for (var n = 0; n < characterLength; n++) {
     password += passwordContents[Math.floor(Math.random() * passwordContents.length)];
-
-  }
-  // return (password);
-  console.log(password);
-  if(passIncludes(password, lettersUC)) {
-    return password
-  } else {
-    generatePassword();
   }
 
-  function passIncludes(arr1, arr2) {
-
+  // ensure all criteria are met
+  function passIncludesAll(arr1, arr2) {
+  
     for(var i = 0; i < arr1.length; i++) {
-
+      
       for(var j = 0; j < arr2.length; j++) {
 
-        if(arr1[i] == arr2[j]) {
+        if(arr1[i].includes(arr2[j])) {
+          console.log("True");
           return true;
         }
       }
     }
     // console.log('False');
+    console.log("False");
     return false;
   }
+
+  // finally, DISPLAY the password
+  console.log(password);
+  if(passIncludesAll(password.split(""), numbers)) {
+    return password
+  } else {
+    return "doesn't contain all criteria";
+  }
+
 
 }
 
